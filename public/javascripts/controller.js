37a516b65a78f0707d88275f1154b9eef7109603
Ning <jiangbuf@gmail.com>
2015-07-20 13:44:05 -0700

    angularjs shopping cart

---
+++ public/javascripts/controller.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+
+// the storeController contains two objects:
+// - store: contains the product list
+// - cart: the shopping cart object
+function storeController($scope, $routeParams, DataService) {
+
+    // get store and cart from service
+    $scope.store = DataService.store;
+    $scope.cart = DataService.cart;
+	$scope.results = DataService.searchresults;
+	
+	$scope.$watch(function () { return DataService.searchresults }, function (newVal, oldVal) {
+		$scope.results = DataService.searchresults;
+	});
+
+	$scope.addCart = function(item) {
+		console.log("Add item");
+		$scope.cart.addItem(item._id, item.name, item.price, item.quantity);
+	}
+	
+    // use routing to pick the selected product
+    if ($routeParams.productSku != null) {
+        $scope.product = $scope.store.getProduct($routeParams.productSku);
+    }
+}
+
+function searchResultsController($scope, $routeParams, DataService, data) {
+	if (data != null) {
+		data.data.forEach(function(item) {item.quantity = 1;});
+		$scope.results = data.data;
+	}
+	$scope.cart = DataService.cart;
+	$scope.store = DataService.store;
+	
+	$scope.addCart = function(item) {
+		console.log("Add item");
+		$scope.cart.addItem(item._id, item.name, item.price, item.quantity);
+	}
+}
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,20 +1,129 @@
-var app = angular.module('pgApp', []);
+// App Module: the name AngularStore matches the ng-app attribute in the main <html> tag
+// the route provides parses the URL and injects the appropriate partial page
+var app = angular.module('pgApp', ['ngRoute']);
+app.config(function($routeProvider) {
+  $routeProvider.
+	  when('/searchresults',{
+		 templateUrl: '/searchresults',
+		 controller: searchResultsController,
+		 resolve: {
+			data: function ($q, $http) {
+					var deferred = $q.defer();
+					var keywords = $("#inputSearch").val();
+					if (keywords.trim().length > 0) {
+						var url = "/search?term=" + keywords;
+						$http({method: 'GET', url: url}).then(function(data) {
+						  deferred.resolve(data);
+						});
+					}
+					else 
+						deferred.resolve(null);
+					return deferred.promise;
+			}
+		 }
+	  }).
+      when('/store', {
+        templateUrl: '/store',
+        controller: storeController 
+      }). 
+      when('/products/:productSku', {
+        templateUrl: '/product',
+        controller: storeController
+      }).
+      when('/cart', {
+        templateUrl: '/cart',
+        controller: storeController
+      }).
+      otherwise({
+        redirectTo: '/store'
+      });
+});
 
-app.controller("pgController", function($scope, $rootScope, $sce, $http, $window) {
+// create a data service that provides a store and a shopping cart that
+// will be shared by all views (instead of creating fresh ones for each view).
+app.factory("DataService", function () {
+
+    // create store
+    var myStore = new store();
+
+    // create shopping cart
+    var myCart = new shoppingCart("AngularStore");
+	
+	var searchResults = null;
+
+    // enable PayPal checkout
+    // note: the second parameter identifies the merchant; in order to use the 
+    // shopping cart with PayPal, you have to create a merchant account with 
+    // PayPal. You can do that here:
+    // https://www.paypal.com/webapps/mpp/merchant
+    myCart.addCheckoutParameters("PayPal", "paypaluser@youremail.com");
+
+    // enable Google Wallet checkout
+    // note: the second parameter identifies the merchant; in order to use the 
+    // shopping cart with Google Wallet, you have to create a merchant account with 
+    // Google. You can do that here:
+    // https://developers.google.com/commerce/wallet/digital/training/getting-started/merchant-setup
+    myCart.addCheckoutParameters("Google", "xxxxxxx",
+        {
+            ship_method_name_1: "UPS Next Day Air",
+            ship_method_price_1: "20.00",
+            ship_method_currency_1: "USD",
+            ship_method_name_2: "UPS Ground",
+            ship_method_price_2: "15.00",
+            ship_method_currency_2: "USD"
+        }
+    );
+
+    // enable Stripe checkout
+    // note: the second parameter identifies your publishable key; in order to use the 
+    // shopping cart with Stripe, you have to create a merchant account with 
+    // Stripe. You can do that here:
+    // https://manage.stripe.com/register
+    myCart.addCheckoutParameters("Stripe", "pk_test_xxxx",
+        {
+            chargeurl: "https://localhost:1234/processStripe.aspx"
+        }
+    );
+
+    // return data object with store and cart
+    return {
+        store: myStore,
+        cart: myCart,
+		searchresults: searchResults
+    };
+});
+
+// Directive detects finish of data binding
+app.directive('myPostRepeatDirective', function() {
+    return function(scope, element, attrs) {
+      if (scope.$last) {
+        scope.$eval('doSearchComplete()');
+      }
+    };
+});
+  
+app.controller("pgController", function($scope, $rootScope, $sce, $http, $window, $location, DataService) {
 	$scope.results = null;
-	$scope.search = function() {
+	$scope.cart = DataService.cart;
+	
+	$scope.searchProducts = function() {
 		var keywords = $("#inputSearch").val();
 		var url = "/search?term=" + keywords;
+		if (keywords.trim().length > 0)
 		var response = $http.get(url);
 		response.success(function(data) {
-			$scope.results = data;
+			if (data != null)
+				data.forEach(function(item) {
+					item.quantity = 1;
+				});
+			DataService.searchresults = data;
+			if ($location.url().indexOf("store") < 0)
+				$location.path('/#/store');
 		});
 		response.error(function() {
 			
 		});
 	}
 	
-	$scope.addCart = function(item) {
-		
-	}
+	
 });
